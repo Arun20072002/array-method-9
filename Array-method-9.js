42bcf813e1ca9cba013cfd6b5fe19dc3c4e972fa
Arunkum20072002 <arunkumarmarikani10@gmail.com>
2023-12-04 11:38:56 +0530

    Array-method

---
+++ Array-method-9.js
@@ -0,0 +1,11 @@
+document.write("9.Remove Banana and add Pineapple in the end of array"+"<br>")
+document.write("------------------------------------------------------"+"<br>")
+
+var fruits = ["Banana","Orange","Apple","Mango"];
+document.write(fruits+"<br>")
+
+var a=fruits.shift(0)
+document.write(fruits+"<br>")
+
+var b=fruits.push("Pineapple")
+document.write(fruits+"<br>")
